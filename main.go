65fe93abb259974bc21bb950bfbaaeb375e30310
Will Chen <will.chen936@gmail.com>
2025-08-24 21:07:25 +0800

    feat: Connect database with pgx5

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	conn, err := pgx.Connect(context.Background(), "postgres://root:pass.123@localhost:5432/moneytor")
+	if err != nil {
+		fmt.Printf("Unable to connect to database: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var id int
+	var code string
+	err = conn.QueryRow(context.Background(), "SELECT * FROM currencies WHERE id=$1", 10).Scan(&id, &code)
+	if err != nil {
+		fmt.Printf("Query ro failed: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(id, code)
 }
