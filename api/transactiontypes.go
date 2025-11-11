ab42132568651ac03f92f767d4c62898c70c5632
will.chen <will.chen936@gmail.com>
2025-11-11 15:23:06 +0800

    feat: Add api "GET transactiontypes"

---
+++ api/transactiontypes.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (server *Server) listTransactionTypes(ctx *gin.Context) {
+	transcationTypes, err := server.queries.ListTransactionTypes(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, transcationTypes)
+}
