d093253878d1ca1d051dff32277676c32e3fb93b
will.chen <will.chen936@gmail.com>
2025-11-11 13:52:44 +0800

    feat: Add validation for api "POST categories"

--- api/category.go
+++ api/category.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	db "moneytor/database/sqlc"
 	"net/http"
 
@@ -19,8 +20,6 @@ func (server *Server) createCategory(ctx *gin.Context) {
 		return
 	}
 
-	// Todo: Check if passed transactionTypeID is leagal.
-
 	arg := db.CreateCategoryParams{
 		Name:              req.Name,
 		TransactionTypeID: req.TransactionTypeID,
@@ -28,6 +27,10 @@ func (server *Server) createCategory(ctx *gin.Context) {
 
 	category, err := server.queries.CreateCategory(ctx, arg)
 	if err != nil {
+		if db.ErrorCode(err) == db.ForeignKeyViolation {
+			ctx.JSON(http.StatusNotFound, errResponse(errors.New("transaction type not found")))
+			return
+		}
 		ctx.JSON(http.StatusInternalServerError, errResponse(err))
 		return
 	}
