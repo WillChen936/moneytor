c7afaca80241005adad82c3cce413c7b7ac03ae9
will.chen <will.chen936@gmail.com>
2026-02-18 22:31:34 +0800

    chore: remove unused code
    - Delete api/decimal.go (Decimal type and UnmarshalJSON were never used)
    - Remove RandomInt32Range and RandomDecimalRange from utils/random.go
    - Drop unused shopspring/decimal import from utils/random.go

--- utils/random.go
+++ utils/random.go
@@ -3,8 +3,6 @@ package utils
 import (
 	"math/rand"
 	"strings"
-
-	"github.com/shopspring/decimal"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -17,27 +15,12 @@ func RandomInt16Range(min, max int16) int16 {
 	return min + int16(rand.Int31n(int32(max-min+1)))
 }
 
-func RandomInt32Range(min, max int32) int32 {
-	if max <= min {
-		return min
-	}
-
-	return min + rand.Int31n(max-min+1)
-}
-
 func RandomInt64Range(min, max int64) int64 {
 	if max <= min {
 		return min
 	}
 
 	return min + rand.Int63n(max-min+1)
-}
-
-func RandomDecimalRange(min, max int64, decimalPlaces int32) decimal.Decimal {
-	randInt := RandomInt64Range(min, max)
-	divisor := decimal.New(1, decimalPlaces)
-
-	return decimal.NewFromInt(randInt).Div(divisor)
 }
 
 func RandomString(length int) string {
